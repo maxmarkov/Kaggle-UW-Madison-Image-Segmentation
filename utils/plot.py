cc3589e679c9f90a2c10f010fbf9d4d24641964e
maksim.markov1 <maksim.markov@kapernikov.com>
2022-05-10 23:07:32 +0200

    EDA notebook started

--- utils/plot.py
+++ utils/plot.py
@@ -14,3 +14,41 @@ def open_gray16(_path, normalize=True, to_rgb=False):
             return np.tile(np.expand_dims(cv2.imread(_path, cv2.IMREAD_ANYDEPTH), axis=-1), 3)
         else:
             return cv2.imread(_path, cv2.IMREAD_ANYDEPTH)
+
+# ref: https://www.kaggle.com/paulorzp/run-length-encode-and-decode
+# modified from: https://www.kaggle.com/inversion/run-length-decoding-quick-start
+def rle_decode(mask_rle, shape, color=1):
+    """ TBD
+    
+    Args:
+        mask_rle (str): run-length as string formated (start length)
+        shape (tuple of ints): (height,width) of array to return 
+    
+    Returns: 
+        Mask (np.array)
+            - 1 indicating mask
+            - 0 indicating background
+
+    """
+    # Split the string by space, then convert it into a integer array
+    s = np.array(mask_rle.split(), dtype=int)
+
+    # Every even value is the start, every odd value is the "run" length
+    starts = s[0::2] - 1
+    lengths = s[1::2]
+    ends = starts + lengths
+
+    # The image image is actually flattened since RLE is a 1D "run"
+    if len(shape)==3:
+        h, w, d = shape
+        img = np.zeros((h * w, d), dtype=np.float32)
+    else:
+        h, w = shape
+        img = np.zeros((h * w,), dtype=np.float32)
+
+    # The color here is actually just any integer you want!
+    for lo, hi in zip(starts, ends):
+        img[lo : hi] = color
+        
+    # Don't forget to change the image back to the original shape
+    return img.reshape(shape)
